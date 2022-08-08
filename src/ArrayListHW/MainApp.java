2ff5c6b55778bab2e548e11b168417b13f983472
BlackBullet19 <o.gercens@gmail.com>
2022-08-08 16:09:26 +0200

    Создать два ArrayList , заполнить их числами, упорядочить каждый и объединить в один ArrayList в упорядоченном виде. Результат вывести в консоль.

---
+++ src/ArrayListHW/MainApp.java
@@ -0,0 +1,39 @@
+package ArrayListHW;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        MainApp main = new MainApp();
+
+        List<Integer> list1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list1.add(main.randomValue());
+        }
+
+        Collections.sort(list1);
+
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list2.add(main.randomValue());
+        }
+
+        Collections.sort(list2);
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.addAll(list1);
+        list3.addAll(list2);
+
+        System.out.println();
+        for (Integer value : list3) {
+            System.out.println(value);
+        }
+    }
+
+    private int randomValue() {
+        return (int) (Math.random() * 100);
+    }
+}
